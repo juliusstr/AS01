a61f320f2644e6b74de539f0e158e4e0b71a03e0
juliusstr <julius@struwing.dk>
2022-09-14 15:58:09 +0200

    Add files via upload

---
+++ myshell.c
@@ -0,0 +1,45 @@
+ #include<stdio.h>
+
+ #define TRUE 1
+
+ void type_prompt();
+ void readCommand(char* com, char* para);
+ void waitpid(int a , char* sta, int b);
+
+ int main(int argc, char const *argv[])
+ {
+    char command[50], parameters[500];
+    char * status;
+    while(TRUE) {
+        type_prompt();
+        readCommand(command, parameters);
+
+        if (fork()  != 0){
+            waitpid(-1,status, 0);
+        } else {
+            execve(command, parameters, 0);
+        }
+    } 
+    return 0;
+ }
+ 
+void type_prompt(){
+    char * text = {"Type command: "};
+    printf("%s", text);
+    return;
+}
+
+void readCommand (char* com, char* para) {
+    char temp[500];
+    int spaceChar = (int) ' ';
+    scanf("%499s", temp);
+
+    //printf("%s", temp);
+    
+    return;
+}
+
+ void waitpid(int a , char* sta, int b) {
+    return;
+ }
+ 
